539aaca7964b584f8d020a9c7457e4f5bc9f5b0e
cxmerondeal <81758891+cxmerondeal@users.noreply.github.com>
2021-10-07 16:18:49 -0500

    Add files via upload

--- Midterm.R
+++ Midterm.R
@@ -63,18 +63,23 @@ defense_score <- function(possession, sector){
 
 
 ###Possession flip function- NEED HELP- just stays stuck on the same values
-possession_flip <- function(possession){
-  if(possession==1) {
-    ball_possession <- 2
-  } else if(possession==2) {
-    ball_possession <- 1
+possession_flip <- function(){
+  if(ball_possession==1) {
+    ball_possession = 2
+  } else if(ball_possession==2) {
+    ball_possession = 1
   }
+  return(ball_possession)
 }
+
 ball_possession=1
-possession_flip(possession = as.numeric(ball_possession))
+possession_flip()
 ball_possession
 possession_flip(possession = as.numeric(ball_possession))
 ball_possession
+
+
+
 
 
 
@@ -90,10 +95,7 @@ if ((def_team_roll>off_team_roll) & (ball_possession==1)) {
 } else ###Next
   
 ###2. Attacking team moves
-if ((ball_possession==1 & sector=4) | (ball_possession==2 & sector=1)) {
-  close_shot <-1
-}
-if (close_shot==1){
+if ((ball_possession==1 & sector==4) | (ball_possession==2 & sector==1)) {
 sector_players <- team_1$offense_score[team_1$sector==sector]
 lst_sample(sector_players, 1, replace = T)
 }
@@ -130,3 +132,119 @@ if (pass_successful == 1) {
 
 
 
+
+
+
+
+#####Cameron's contribution
+
+#####Probability Constants:
+coin_flip_prob=0.5
+long_shot_prob=0.3
+off_side_prob=0.15
+goalie_pass_sec2=0.6
+
+###Create Player Score Lists
+team_1 <- list("player_number"=(1:10), "sector"=c(1,1,1,2,2,2,2,3,3,4), "offense_score"=c(1:10), "defense_score"=c(10:1), "points_score"=c(0))
+team_2 <- list("player_number"=(1:10), "sector"=c(4,4,3,3,3,2,2,2,1,1), "offense_score"=c(1:10), "defense_score"=c(10:1), "points_score"=c(0))
+
+#################DEFINING FUNCTIONS
+
+
+###Defining lst_sample
+lst_sample <- function(x, size, replace = F) {
+  if (length(x) == 1) return(rep(x, size))
+  base::sample(x, size = size, replace = replace)
+}
+
+###Offense and Defense Score Functions
+offense_score <- function(possession, sector){
+  if (possession==1){
+    N <- which(team_1$sector==sector)
+    offense_score1 = 0
+    for (i in N){
+      offense_score1 <- offense_score1 + team_1$offense_score[i]
+    }
+  } else {
+    N <- which(team_2$sector==sector)
+    offense_score1 = 0
+    for (i in N){
+      offense_score1 <- offense_score1 + team_2$offense_score[i]
+    }
+  }
+  return(offense_score1)
+}
+###Test: offense_score(possession = 1, sector = 2)
+
+defense_score <- function(possession, sector){
+  if (possession==2){
+    N <- which(team_1$sector==sector)
+    defense_score1 = 0
+    for (i in N){
+      defense_score1 <- defense_score1 + team_1$defense_score[i]
+    }
+  } else {
+    N <- which(team_2$sector==sector)
+    defense_score1 = 0
+    for (i in N){
+      defense_score1 <- defense_score1 + team_2$defense_score[i]
+    }
+  }
+  return(defense_score1)
+}
+
+###Flipping the coin
+coin_flip <- runif(1, min=0, max=1)
+if (coin_flip>coin_flip_prob) {
+  ball_possession=1
+  sector=2
+} else {ball_possession=2 
+sector=3}
+
+###Defense Tries to Take the Ball
+off_sum <- offense_score(possession = ball_possession, sector = sector)
+def_sum <- defense_score(possession = ball_possession, sector = sector)
+off_team_roll <- runif(1, min=1, max=off_sum)
+def_team_roll <- runif(1, min=1, max=def_sum)
+if ((def_team_roll>off_team_roll) & (ball_possession==1)) {
+  ball_possession <- 2
+  change <-1
+  if (change==1) break
+} else if ((def_team_roll>off_team_roll) & (ball_possession==2)) {
+  ball_possession <- 1
+  change <-1
+  if (change==1) break
+} else next
+
+###Attacking team 
+if (ball_possession==1 & sector==4) {
+  sector_offplayers <- team_1$offense_score[team_1$sector==sector]
+  off_score <- lst_sample(sector_offplayers, 1, replace = T)
+  off_team_roll <- runif(1, min=1, max=off_score)
+  goalie_roll <- runif(1, min=1, max=7)
+  if(off_team_roll>goalie_roll) {
+    team_1$points_score <- team_1$points_score + 1
+    #Possession flips and the minute ends
+    change <-1
+    if (change==1) break
+  } else {
+    #possession flips and the minute ends
+    change <-1
+    if (change==1) break
+  }
+} else if (ball_possession==2 & sector==1) {
+  sector_offplayers <- team_2$offense_score[team_2$sector==sector]
+  off_score <- lst_sample(sector_offplayers, 1, replace = T)
+  off_team_roll <- runif(1, min=1, max=off_score)
+  goalie_roll <- runif(1, min=1, max=7)
+  if(off_team_roll>goalie_roll) {
+    team_1$points_score <- team_1$points_score + 1
+    #Possession flips and the minute ends
+    change <-1
+    if (change==1) break
+  } else {
+    #possession flips and the minute ends
+    change <-1
+    if (change==1) break
+  }
+  } else next
