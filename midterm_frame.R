785b650e90390c7f2dc193a99cef89416322028d
cxmerondeal <81758891+cxmerondeal@users.noreply.github.com>
2021-10-07 14:31:15 -0500

    Add files via upload

---
+++ midterm_frame.R
@@ -0,0 +1,26 @@
+#Define probability constants
+
+#Define the functions
+
+#Monte Carlo
+
+#Game Function
+
+
+#flip coin stuff
+
+
+
+for (i in 1:90){
+#Goalie possession stuff
+  
+#Defense tries to take the ball
+  #End if successful
+#If in attacking sector 4
+  #close shot
+#Long shot (attacking sector 3)
+  #offsides
+#Pass ball forward
+}
+
+##Print score and winner
