b4200187027cd14664f274a61b2c33a8fcbc8d3f
Vikram Meyer <vjmeyer20@gmail.com>
2021-10-12 09:58:49 -0500

    more fixes

--- midterm_solution.R
+++ midterm_solution.R
@@ -1,3 +1,5 @@
+# Zoe, Cameron, Vikram
+
 # Probability constants from problem description
 OFFSIDE_PROB = 0.15
 LONGSHOT_PROB = 0.3
@@ -48,23 +50,25 @@ defense_score <- function(team_1,team_2,game_state){
 minute <- function(team_1, team_2, game_state) {
   #Goalie possession stuff
   if (game_state$goalie_with_ball == TRUE){
-  #pass to sector 2 with probability 0.6
-  goalieKick = runif(1, 0, 1)
-   if (goalieKick <= PASS_TO_SECTOR2_PROB & game_state$team_possession == 1){
-     game_state$ball_in_sector = 2
-   } else if (goalieKick <= PASS_TO_SECTOR2_PROB & game_state$team_possession == 2){
-     game_state$ball_in_sector = 3
-   } else { 
-     #otherwise pass to sector (attacking) 3
-     if (game_state$team_posession == 1){
-       game_state$ball_in_sector = 3
-     } else {
-       game_state$ball_in_sector = 2
-     }
+    #pass to sector 2 with probability 0.6
+    goalieKick = runif(1, 0, 1)
+    if (goalieKick <= PASS_TO_SECTOR2_PROB & game_state$team_possession == 1){
+      game_state$ball_in_sector = 2
+    } 
+    else if (goalieKick <= PASS_TO_SECTOR2_PROB & game_state$team_possession == 2){
+      game_state$ball_in_sector = 3
+    } 
+    else { 
+      #otherwise pass to sector (attacking) 3
+      if (game_state$team_posession == 1){
+        game_state$ball_in_sector = 3
+      } else {
+        game_state$ball_in_sector = 2
+      }
      #defense tries to overtake ball
      offense_roll = runif(1,1,offense_score(team_1,team_2,game_state))
      defense_roll = runif(1,1,defense_score(team_1,team_2,game_state))
-     #if sucessfully overtakes ball, change possession
+     #if successfully overtakes ball, change possession
      if (defense_roll > offense_roll) { 
         if (game_state$team_possession == 1){
           game_state$team_possession == 2
@@ -74,67 +78,74 @@ minute <- function(team_1, team_2, game_state) {
       cat('Intercept after the kick, team ', game_state$team_possession,
           ' is now in possession')
     }
+    }
   }
-}
   
   #Defense tries to take the ball- Cameron
-  off_sum <- offense_score(possession = game_state$team_possession, sector = game_state$ball_in_sector)
-def_sum <- defense_score(possession = game_state$team_possession, sector = game_state$ball_in_sector)
-off_team_roll <- runif(1, min=1, max=off_sum)
-def_team_roll <- runif(1, min=1, max=def_sum)
-if ((def_team_roll>off_team_roll) & (game_state$team_possession==1)) {
-  game_state$team_possession <- 2
-  game_state$cur_minute <- game_state$cur_minute + 1
-  return(game_state)
-} else if ((def_team_roll>off_team_roll) & (game_state$team_possession==2)) {
-  game_state$team_possession <- 1
-  game_state$cur_minute <- game_state$cur_minute + 1
-  return(game_state)
-} 
+  off_sum <- offense_score(team_1,team_2,game_state)
+  def_sum <- defense_score(team_1,team_2,game_state)
+  off_team_roll <- runif(1, min=1, max=off_sum)
+  def_team_roll <- runif(1, min=1, max=def_sum)
+  if ((def_team_roll>off_team_roll) & (game_state$team_possession==1)) {
+    game_state$team_possession <- 2
+    game_state$cur_minute <- game_state$cur_minute + 1
+    return(game_state)
+  } else if ((def_team_roll>off_team_roll) & (game_state$team_possession==2)) {
+    game_state$team_possession <- 1
+    game_state$cur_minute <- game_state$cur_minute + 1
+    return(game_state)
+  } 
   
   #If in attacking sector 4- Cameron
-    #close shot- CHANGE the possession and sector variables
+  #close shot- CHANGE the possession and sector variables
   if (game_state$team_possession==1 & game_state$ball_in_sector==4) {
-  sector_offplayers <- team_1$offense_score[team_1$sector==game_state$ball_in_sector]
-  off_score <- lst_sample(sector_offplayers, 1, replace = T)
-  off_team_roll <- runif(1, min=1, max=off_score)
-  goalie_roll <- runif(1, min=1, max=GOALIE_SCORE)
-  cat("Player", team_1$player_number[team_1$offense_score==off_score], "from team 1 takes a shot from sector 4.")
-  if(off_team_roll>goalie_roll) {
-    game_state$score[1] <- game_state$score[1] + 1
-    cat("It's a goal on minute", game_state$cur_minute, "!!! The score is now", game_state$score[1], ":", game_state$score[2])
-  game_state$team_possession <- 2
-  game_state$cur_minute <- game_state$cur_minute + 1
-  return(game_state)
-  } else {
-  game_state$team_possession <- 2
-  game_state$cur_minute <- game_state$cur_minute + 1
-  cat("... and he misses. Team 2 is now in possession")
-  game_state$goalie_with_ball = TRUE
-  game_state$ball_in_sector = 4
-  return(game_state)
-  }
-} else if (game_state$team_possession==2 & game_state$ball_in_sector==1) {
-  sector_offplayers <- team_2$offense_score[team_2$sector==game_state$ball_in_sector]
-  off_score <- lst_sample(sector_offplayers, 1, replace = T)
-  off_team_roll <- runif(1, min=1, max=off_score)
-  goalie_roll <- runif(1, min=1, max=7)
-  cat("Player", team_2$player_number[team_2$offense_score==off_score], "from team 2 takes a shot from sector 1.")
-  if(off_team_roll>goalie_roll) {
-    team_2$points_score <- team_2$points_score + 1
-    cat("It's a goal on minute", i, "!!! The score is now", team_1$points_score, ":", team_2$points_score)
-  game_state$team_possession <- 1
-  game_state$cur_minute <- game_state$cur_minute + 1
-  cat("... and he misses. Team 2 is now in possession")
-  game_state$goalie_with_ball = TRUE
-  game_state$ball_in_sector = 1
-  return(game_state)
+    sector_offplayers <- team_1$offense_score[team_1$sector==game_state$ball_in_sector]
+    off_score <- lst_sample(sector_offplayers, 1, replace = T)
+    off_team_roll <- runif(1, min=1, max=off_score)
+    goalie_roll <- runif(1, min=1, max=GOALIE_SCORE)
+    cat("Player", team_1$player_number[team_1$offense_score==off_score], "from team 1 takes a shot from sector 4.")
+  
+    if(off_team_roll>goalie_roll) {
+      game_state$score[1] <- game_state$score[1] + 1
+      cat("It's a goal on minute", game_state$cur_minute, "!!! The score is now", game_state$score[1], ":", game_state$score[2])
+      game_state$team_possession <- 2
+      game_state$cur_minute <- game_state$cur_minute + 1
+      return(game_state)
+    } else {
+      game_state$team_possession <- 2
+      game_state$cur_minute <- game_state$cur_minute + 1
+      cat("... and he misses. Team 2 is now in possession")
+      game_state$goalie_with_ball = TRUE
+      game_state$ball_in_sector = 4
+      return(game_state)
+    }
+  } else if (game_state$team_possession==2 & game_state$ball_in_sector==1) {
+    sector_offplayers <- team_2$offense_score[team_2$sector==game_state$ball_in_sector]
+    off_score <- lst_sample(sector_offplayers, 1, replace = T)
+    off_team_roll <- runif(1, min=1, max=off_score)
+    goalie_roll <- runif(1, min=1, max=7)
+    
+    cat("Player", team_2$player_number[team_2$offense_score==off_score], "from team 2 takes a shot from sector 1.")
+    
+    if(off_team_roll > goalie_roll) {
+      team_2$points_score <- team_2$points_score + 1
+      cat("It's a goal on minute", game_state$cur_min, "!!! The score is now", game_state$score[1], ":", game_state$score[2])
+    }
+    else {
+      cat("... and he misses. Team 2 is now in possession")
+    }
+    
+    game_state$team_possession <- 1
+    game_state$cur_minute <- game_state$cur_minute + 1
+    game_state$goalie_with_ball = TRUE
+    game_state$ball_in_sector = 1
+    return(game_state)
     
   } else {
-    if (game_state$possession==1) {game_state$possession==2} else {game_state$possession==1}
-  return(game_state)
+    if (game_state$team_possession==1) {game_state$team_possession==2} else {game_state$team_possession==1}
+    return(game_state)
   }
-  } 
+  
   # Try a long shot
   if (game_state$ball_in_sector == 3 & game_state$team_possession == 1 & runif(1,0,1) <= LONGSHOT_PROB) {
     offensive_player = lst_sample(team1$player_number[team_1$sector == 3],1,T)
@@ -160,7 +171,7 @@ if ((def_team_roll>off_team_roll) & (game_state$team_possession==1)) {
   }
   # Otherwise try to pass the ball forward
   else if (game_state$team_possession == 1) {
-    offense_roll = ruinf(1,1,offense_score(team_1,team_2,game_state))
+    offense_roll = runif(1,1,offense_score(team_1,team_2,game_state))
     defense_roll = runif(1,1,defense_score(team_1,team_2,game_state))
       
     if (offense_roll > defense_roll) {
@@ -177,7 +188,7 @@ if ((def_team_roll>off_team_roll) & (game_state$team_possession==1)) {
   }
   # repeat for team 2 
   if (game_state$ball_in_sector == 2 & game_state$team_possession == 2 & runif(1,0,1) <= LONGSHOT_PROB) {
-    offensive_player = lst_sample(team2$player_number[team_1$sector == 2],1,T)
+    offensive_player = lst_sample(team_2$player_number[team_1$sector == 2],1,T)
     cat('Player ',offensive_player,' from team 2 takes a shot from sector 2')
     
     offense_score = min(runif(2,1,team_2$offense_score[offense_player]))
@@ -198,7 +209,7 @@ if ((def_team_roll>off_team_roll) & (game_state$team_possession==1)) {
     return(game_state)
   }
   else if(game_state$team_possession == 2) {
-    offense_roll = ruinf(1,1,offense_score(team_1,team_2,game_state))
+    offense_roll = runif(1,1,offense_score(team_1,team_2,game_state))
     defense_roll = runif(1,1,defense_score(team_1,team_2,game_state))
     
     if (offense_roll > defense_roll) {
@@ -250,3 +261,5 @@ monte_carlo_soccer <- function(N) {
   # in practice we don't need to for the assignment- 1 game loop is enough
   game(minutes = 90)
 }
+
+monte_carlo_soccer();
